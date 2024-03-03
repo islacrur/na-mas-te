e61768c564ced81bbef3de3345306c4bee93112a
islacrur <islacrur@gmail.com>
2024-03-03 20:19:16 +0100

    Update name of public function allergen

--- backend/app/Models/Allergen.php
+++ backend/app/Models/Allergen.php
@@ -10,7 +10,7 @@ class Allergen extends Model
     use HasFactory;
     protected $guarded =  [];
 
-    public function Product(){
+    public function product(){
         return $this->belongsTo(Product::class);
 
     }
