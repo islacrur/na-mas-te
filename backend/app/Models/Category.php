d36a1456a880de0acab8c8c496bf4cb341000a0a
islacrur <islacrur@gmail.com>
2024-03-03 20:16:30 +0100

    Update public funtion name from category model

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -10,7 +10,7 @@ class Category extends Model
     use HasFactory;
     protected $guarded = [];
 
-    public function Product(){
+    public function product(){
         return $this-> hasMany(Product::class); 
     }
 }
