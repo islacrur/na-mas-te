06ea5cdd3e1dc3011c9687c3a66e57b9eb48a046
islacrur <islacrur@gmail.com>
2024-03-03 20:35:32 +0100

    Create product_bill model and migration

---
+++ backend/database/migrations/2024_03_03_192957_create_products_bills_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products_bills', function (Blueprint $table) {
+            $table->unsignedInteger('id_product');
+            $table->unsignedInteger('id_bill');
+            $table->tinyInteger('units');
+            $table->double('price');
+            $table->timestamps();
+
+            $table->foreign('id_product')->references('id')->on('products');
+            $table->foreign('id_bill')->references('id')->on('bills');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products_bills');
+    }
+};
