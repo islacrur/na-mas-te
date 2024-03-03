282dee187d012a2a7f7fcd4d5bcc2b8f9dff2cb6
islacrur <islacrur@gmail.com>
2024-03-03 20:22:39 +0100

    Create bill model and migration

---
+++ backend/database/migrations/2024_03_03_191947_create_bills_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->biginteger('units');
+            $table->double('total_sale_price');
+            $table->date('date');
+            $table->unsignedInteger('id_user');
+            $table->timestamps();
+            
+            $table->foreign('id_user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bills');
+    }
+};
