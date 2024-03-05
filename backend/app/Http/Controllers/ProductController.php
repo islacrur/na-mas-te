87786c09c60882758ab176398706ff91871d78ca
yamiranea <yamilarenea@gmail.com>
2024-03-05 14:52:21 +0100

    create Product Request

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Http\Requests\ProductRequest;
 use Illuminate\Http\JsonResponse;
@@ -16,14 +15,15 @@ class ProductController extends Controller
 
     }
 
-    public function store(ProductRequest $request):JsonResponse
+    public function store(ProductRequest $request): JsonResponse
     {
-        $product = Product::create($request->all());
+        $product = Product::create($request->except('allergens'));
+        $product->allergens()->sync($request->allergens);
+
         return response()->json([
             'success' => true,
             'data' => $product
         ], 201);
-
     }
 
     public function show(string $id):JsonResponse
@@ -32,25 +32,17 @@ class ProductController extends Controller
         return response()->json($product, 200);
     }
 
-    public function update(ProductRequest $request, string $id):JsonResponse
-    {
-        $product = Product::find($id);
-        $product->name = $request->name;
-        $product->description = $request->description;
-        $product->price = $request->price;
-        $product->image = $request->image;
-        $product->allergen = $request->allergen;
-        $product->status = $request->status;
-        $product->category = $request->category;
-        $product->stock = $request->stock;
-        $product->save();
+    public function update(ProductRequest $request, string $id): JsonResponse
+{
+    $product = Product::find($id);
+    $product->update($request->except('allergens'));
+    $product->allergens()->sync($request->allergens);
 
-        return response()->json([
-            'success' => true,
-            'data' => $product
-        ], 200);
-
-    }
+    return response()->json([
+        'success' => true,
+        'data' => $product
+    ], 200);
+}
 
     public function destroy(string $id):JsonResponse
     {
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,4 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('/products', ProductController::class);
