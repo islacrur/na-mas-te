f79fd36489768548059817c248a4c4bd97e20333
yamiranea <yamilarenea@gmail.com>
2024-03-08 15:07:01 +0100

    Create BillController logic functions

--- backend/app/Http/Controllers/BillController.php
+++ backend/app/Http/Controllers/BillController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Bill;
+use App\Models\Product;
 
 class BillController extends Controller
 {
-    //
+public function addToCart(Request $request)
+{
+    $validatedData = $request->validate([
+        'id_product' => 'required|exists:products,id',
+        'units' => 'required|integer|min:1',
+    ]);
+
+    $product = Product::find($validatedData['id_product']);
+
+    $bill = Bill::firstOrCreate(['status' => 'Processing']);
+
+    $productBill = $bill->products_bills()->create([
+        'id_product' => $product->id,
+        'units' => $validatedData['units'],
+        'total_sale_price' => $product->price * $validatedData['units'],
+    ]);
+
+    return response()->json($productBill, 201);
+}
 }
