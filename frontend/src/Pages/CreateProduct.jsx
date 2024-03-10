a9d0dc5901996be638b580c62a72a3ffbd486bb7
mariamf1985 <mariamf1985@gmail.com>
2024-03-10 11:46:14 +0100

    create CreateProduct

---
+++ frontend/src/Pages/CreateProduct.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CreateProduct = () => {
+  return (
+    <>
+    <h1>Crear un producto</h1>
+      <Navbar></Navbar>
+      <ProductCard></ProductCard>
+      <Footer></Footer>
+   </>
+  )
+}
+
+export default CreateProduct
